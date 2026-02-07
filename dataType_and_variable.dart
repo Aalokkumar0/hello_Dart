eb28c9a23e760c45301f0f4cc7cf395770e30bcc
Aalok_kr. <121732315+Aalokkumar0@users.noreply.github.com>
2026-02-07 07:54:13 +0000

    Variable

---
+++ dataType_and_variable.dart
@@ -0,0 +1,16 @@
+/* 
+        Variable:-
+                 it can store something or Contain something
+                 and it can veri also means it can change also 
+
+                        Eg: you can add the value in variable 
+                                old value var = 20;
+                                new value var = 25; So it can change 
+
+
+            ** How to deffine variable **
+
+
+
+
+*/
